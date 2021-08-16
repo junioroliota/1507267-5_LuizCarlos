97f74e30b55b5c3bd09c017ecb61049a05764b0f
junioroliota <88904722+junioroliota@users.noreply.github.com>
2021-08-16 15:15:09 -0300

    Add files via upload

--- dados/dados.php
+++ dados/dados.php
@@ -1,10 +1,16 @@
 <?php
 	$produtos = array(
-		'Trufa Chocolate' =>'Images\trufa1.jpg',
-		'Trufa Maracuja' =>'Images\trufa2.jpg',
-		'Trufa Coco' =>'Images\trufa3.jpg',
-		'Trufa Morango' =>'Images\trufa4.jpg',
+		'pages\Trufa_Chocolate.html' =>'Images\trufa1.jpg',
+		'pages\Trufa_Maracuja.html' =>'Images\trufa2.jpg',
+		'pages\Trufa_Coco.html' =>'Images\trufa3.jpg',
+		'pages\Trufa_Morango.html' =>'Images\trufa4.jpg',
 	);
 
+	$sabor = array(
+		'pages\Trufa_Chocolate.html' =>'Trufa de Chocolate',
+		'pages\Trufa_Maracuja.html' =>'Trufa de Maracuja',
+		'pages\Trufa_Coco.html' =>'Trufa de Coco',
+		'pages\Trufa_Morango.html' =>'Trufa de Morango',
+	);
 	
 ?>
